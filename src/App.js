8bbb1085f4983b53c2f4f182cb5998617066b1a9
johnmuthua <caveomuthama@gmail.com>
2020-09-11 19:02:12 +0300

    added a header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Switch>
+        <div className="App">
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login</h1>
+          </Route>
+          <Route exact path="/">
+            <Header />
+          </Route>
+        </div>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
